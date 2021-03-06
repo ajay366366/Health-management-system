8a9a2791efa8f8e0acc33d8f4e50aef23441190d
ajay <akajaykumar0232@gmail.com>
2021-03-06 11:48:12 +0530

    code at initial stage

---
+++ practice.py
@@ -0,0 +1,50 @@
+# to play mp3
+"""from pygame import mixer
+mixer.init()
+mixer.music.load("tinny.mp3")
+mixer.music.set_volume(0.7)
+mixer.music.play()
+"""
+#waiting time
+"""import time
+time.sleep(50)
+print("a")"""
+
+"""
+import time
+initil_time =time.time()
+i=1
+while True :
+    final_time=time.time()
+    time_diff= int(final_time-initil_time)
+    if time_diff==2 and i<=5:
+        print(f" {initil_time} {i} done")
+        i=i+1
+        initil_time=final_time
+    else:
+        if i<=5:
+            continue
+        else:
+            break
+
+
+
+"""
+i=0
+while True:
+    i=i+1
+    if i<5:
+        print("done")
+    elif i==3:
+        print ("aj")
+    else:
+        print("pp")
+        break
+"""import time
+initial_time =time.time()
+while True:
+    for
+        print ("30seconds passed")
+    else:
+        i=i+1
+        continue"""
